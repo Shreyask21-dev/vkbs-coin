7b2c87ccfbcc87fb34243ce4f32fa9f07c2a6124
Sagar Shirsat <vatsalyasagar8@gmail.com>
2024-03-06 18:47:13 +0530

    mobile freindly

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="icon" href='/images/favicon.png' sizes="40x40" />
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" crossOrigin="anonymous" />
-       <meta http-equiv="cache-control" content="no-cache" />
-       <meta http-equiv="expires" content="0" />
-       <meta http-equiv="pragma" content="no-cache" />
-        </Head>
+        <link rel="icon" href='/images/favicon.png' sizes="40x40" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" crossOrigin="anonymous" />
+        <meta http-equiv="cache-control" content="no-cache" />
+        <meta http-equiv="expires" content="0" />
+        <meta http-equiv="pragma" content="no-cache" />
+      </Head>
       <body>
         <Main />
         <NextScript />
