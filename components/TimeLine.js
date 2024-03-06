b452db1adc0b0fffb6e07ed6bf994492129d9f78
Sagar Shirsat <vatsalyasagar8@gmail.com>
2024-03-06 17:35:17 +0530

    added loader

--- components/TimeLine.js
+++ components/TimeLine.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
 
+
 export default function TimeLine() {
+
+  const events = [
+    { year: 2001, event: 'Event 1' },
+    { year: 2003, event: 'Event 2' },
+    { year: 2005, event: 'Event 3' },
+    { year: 2008, event: 'Event 4' },
+    { year: 2010, event: 'Event 5' },
+  ];
   return (
     <>
     <div className='timeline-section timeline'>
@@ -12,6 +21,18 @@ export default function TimeLine() {
             <img src='/images/time-img.png' alt='' />
           </div>
      </div>
+    </div>
+
+    <div className='container-base'>
+      <h1>Vertical Timeline</h1>
+      <div className='timeline'>
+        {events.map((event, index) => (
+          <div key={index} className='timelineItem'>
+            <div className='timelineYear'>{event.year}</div>
+            <div className='timelineContent'>{event.event}</div>
+          </div>
+        ))}
+      </div>
     </div>
     </>
   )
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,27 @@
 import React, { useEffect } from "react";
+import Router from "next/router";
+import NProgress from "nprogress";
+import Layout from "@/components/Layout";
 import 'bootstrap/dist/css/bootstrap.css'
 import '@/styles/globals.css'
 import '@/styles/homepage.css'
 import '@/styles/multiRangeSlider.css'
 import '@/styles/timeline.css'
-import Layout from "@/components/Layout";
+
 
 export default function App({ Component, pageProps }) {
+
+  Router.onRouteChangeStart = url => {
+    NProgress.start()
+}
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
+
+
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
+
+    
 }, []);
   return (
     <>
