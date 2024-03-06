6e7adafe02c074124a1900b413730887325c7ce6
Sagar Shirsat <vatsalyasagar8@gmail.com>
2024-03-06 17:39:54 +0530

    catche file

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,9 @@ export default function Document() {
       <Head>
       <link rel="icon" href='/images/favicon.png' sizes="40x40" />
       <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" crossOrigin="anonymous" />
-      
+       <meta http-equiv="cache-control" content="no-cache" />
+       <meta http-equiv="expires" content="0" />
+       <meta http-equiv="pragma" content="no-cache" />
         </Head>
       <body>
         <Main />
