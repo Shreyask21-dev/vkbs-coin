eddc9e081d709879fb12852a725bc9e22e8d0e01
vatsalya-sagar <vatsalyasagar8@gmail.com>
2024-09-20 12:06:45 +0530

    css issue fixed

--- pages/_document.js
+++ pages/_document.js
@@ -6,17 +6,19 @@ export default function Document() {
       <Head>
         <link rel="icon" href='/images/favicon.png' sizes="40x40" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" crossOrigin="anonymous" />
+    
        {/* elementor links start */}
-       <link rel='stylesheet' id='elementor-icons-css' href='https://vkbs.coinage.host/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css' media='all' />
-       <link rel='stylesheet' id='elementor-common-css' href='https://vkbs.coinage.host/wp-content/plugins/elementor/assets/css/common.min.css' media='all' />
-       {/* <link rel='stylesheet' id='e-theme-ui-light-css' href='https://vkbs.coinage.host/wp-content/plugins/elementor/assets/css/theme-light.min.css' media='all' /> */}
-       <link rel='stylesheet' id='elementor-frontend-css' href='https://vkbs.coinage.host/wp-content/plugins/elementor/assets/css/frontend-lite.min.css' media='all' />
-       <link rel='stylesheet' id='swiper-css' href='https://vkbs.coinage.host/wp-content/plugins/elementor/assets/lib/swiper/v8/css/swiper.min.css' media='all' />
-       <link rel='stylesheet' id='elementor-post-6-css' href='https://vkbs.coinage.host/wp-content/uploads/elementor/css/post-6.css' media='all' />
-       <link rel='stylesheet' id='elementor-global-css' href='https://vkbs.coinage.host/wp-content/uploads/elementor/css/global.css' media='all' />
+       <link rel='stylesheet' id='elementor-frontend-css' href='https://vkbs.coinage.host/wp-content/plugins/elementor/assets/css/frontend.min.css?ver=3.24.1' media='all' />
+       <link rel='stylesheet' id='swiper-css' href='https://vkbs.coinage.host/wp-content/plugins/elementor/assets/lib/swiper/v8/css/swiper.min.css?ver=8.4.5' media='all' />
+       <link rel='stylesheet' id='e-swiper-css' href='https://vkbs.coinage.host/wp-content/plugins/elementor/assets/css/conditionals/e-swiper.min.css?ver=3.24.1' media='all' />
+       <link rel='stylesheet' id='eihe-front-style-css' href='https://vkbs.coinage.host/wp-content/plugins/image-hover-effects-addon-for-elementor/assets/style.min.css?ver=1.4.4' media='all' />
+       <link rel='stylesheet' id='elementor-global-css' href='https://vkbs.coinage.host/wp-content/uploads/elementor/css/global.css?ver=1726191684' media='all' />
+       <link rel='stylesheet' id='widget-heading-css' href='https://vkbs.coinage.host/wp-content/plugins/elementor/assets/css/widget-heading.min.css?ver=3.24.1' media='all' />
+       <link rel='stylesheet' id='widget-text-editor-css' href='https://vkbs.coinage.host/wp-content/plugins/elementor/assets/css/widget-text-editor.min.css?ver=3.24.1' media='all' />
+       <link rel='stylesheet' id='widget-icon-list-css' href='https://vkbs.coinage.host/wp-content/plugins/elementor/assets/css/widget-icon-list.min.css?ver=3.24.1' media='all' />
+       <link rel='stylesheet' id='widget-image-box-css' href='https://vkbs.coinage.host/wp-content/plugins/elementor/assets/css/widget-image-box.min.css?ver=3.24.1' media='all' />
        <link rel='stylesheet' id='vkbs-style-css' href='https://vkbs.coinage.host/wp-content/themes/vkbs/style.css' media='all' />
         {/* elementor links end */}
-
 
         <meta httpEquiv="cache-control" content="no-cache" />
         <meta httpEquiv="expires" content="0" />
